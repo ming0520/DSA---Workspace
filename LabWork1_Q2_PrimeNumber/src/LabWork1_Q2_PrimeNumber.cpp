2803bf604704b1e7251cca8c4f4257ae10df0104
ming0520 <mingblog0520@gmail.com>
2019-11-24 21:57:20 +0800

    Update LabWork1_Q2_PrimeNumnber v2 release

--- LabWork1_Q2_PrimeNumber/src/LabWork1_Q2_PrimeNumber.cpp
+++ LabWork1_Q2_PrimeNumber/src/LabWork1_Q2_PrimeNumber.cpp
@@ -6,65 +6,36 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 #include <iostream>
-#include <algorithm>
-// SET CAPACITY value for n-number
-#define CAPACITY 5500
+#include <bits/stdc++.h>
 using namespace std;
 
-/*
- * Purpose: Search desired value via binary search in recursive
- */
-int binarySearch(int arr[], int first, int last, int search){
-	if(last >= 1){
-		int middle = (first + last - 1) / 2;
-		if(arr[middle] == search){
-			return middle;
+bool * SOE (int n){
+	static bool isPrime[5500];
+	memset(isPrime,true,sizeof(isPrime));
+
+	for(int i = 2; i*i <= n; i++){
+		if(isPrime[i] == true){
+			for(int j = i*2; j <= n; j+=i){
+				isPrime[j] = false;
+			}
 		}
-		else if(arr[middle] < search){
-			return binarySearch(arr, middle + 1, last, search);
-		}
-		return binarySearch(arr, first, middle - 1, search);
-	}
-	return -1;
-}
-/*
- * Purpose: Display desired value in array in recursive
- */
-int recurPrint(int arr[], int start, int n){
-	//back to previous function when start value
-	//greater or equal to end value
-	if(start >= n){
-		return 0;
 	}
 
-		std::cout << arr[start] << " ";
-		//print next value, increase start value by 1
-		return recurPrint(arr,start+1,n);
+	return isPrime;
 }
 int main (){
-	int n = 30;
-	int list[CAPACITY];
-	//Store all the possible value into array until number-n
-	for(int i = 1; i <= n; i++){
-		list[i-1] = i;
-	}
-	//remove all non prime number (Sieve of Eratosthenes)
-	for (int i = 1; i <= n; i++){
-		for(int j = i+1; j <=n; j++){
-			if(list[i] == 0){
-				break;
-			}
-			int remainder = list[j] % list[i];
-			if(remainder == 0){
-				list[j] = 0;
-			}
+	int n = 0;
+	bool *arr;
+	std::cout << "Enter n-number: ";
+	cin >> n;
+
+	arr = SOE(n);
+
+	for(int i = 2; i <= n; i++){
+		if(arr[i]){
+			cout << i <<endl ;
 		}
 	}
-
-	std::sort(std::begin(list), std::end(list));
-	int firstPrime = binarySearch(list, 0, n, 1);
-	recurPrint(list,firstPrime, n);
-
 	return 0;
 }
 
