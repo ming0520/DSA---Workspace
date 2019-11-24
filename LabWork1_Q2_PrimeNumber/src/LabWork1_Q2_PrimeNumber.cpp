24afc1c967d1a5df4d745c01d720f6630a0b1209
ming0520 <mingblog0520@gmail.com>
2019-11-24 21:03:35 +0800

    Update LabWork1_Q2_PrimeNumber.cpp v1 release

--- LabWork1_Q2_PrimeNumber/src/LabWork1_Q2_PrimeNumber.cpp
+++ LabWork1_Q2_PrimeNumber/src/LabWork1_Q2_PrimeNumber.cpp
@@ -6,18 +6,49 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 #include <iostream>
-#define CAPACITY 30
+#include <algorithm>
+// SET CAPACITY value for n-number
+#define CAPACITY 5500
 using namespace std;
 
-int main (){
-	int n = 0;
-	int list[CAPACITY];
+/*
+ * Purpose: Search desired value via binary search in recursive
+ */
+int binarySearch(int arr[], int first, int last, int search){
+	if(last >= 1){
+		int middle = (first + last - 1) / 2;
+		if(arr[middle] == search){
+			return middle;
+		}
+		else if(arr[middle] < search){
+			return binarySearch(arr, middle + 1, last, search);
+		}
+		return binarySearch(arr, first, middle - 1, search);
+	}
+	return -1;
+}
+/*
+ * Purpose: Display desired value in array in recursive
+ */
+int recurPrint(int arr[], int start, int n){
+	//back to previous function when start value
+	//greater or equal to end value
+	if(start >= n){
+		return 0;
+	}
 
-	n = 30;
+		std::cout << arr[start] << " ";
+		//print next value, increase start value by 1
+		return recurPrint(arr,start+1,n);
+}
+int main (){
+	int n = 30;
+	int list[CAPACITY];
+	//Store all the possible value into array until number-n
 	for(int i = 1; i <= n; i++){
 		list[i-1] = i;
 	}
-
+	//remove all non prime number (Sieve of Eratosthenes)
 	for (int i = 1; i <= n; i++){
 		for(int j = i+1; j <=n; j++){
 			if(list[i] == 0){
@@ -30,13 +61,10 @@ int main (){
 		}
 	}
 
-	for(int i = 0; i < n/10 ; i++){
-		for(int j = 0 ; j < 10; j++){
-			cout << list[(i*10)+j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
+	std::sort(std::begin(list), std::end(list));
+	int firstPrime = binarySearch(list, 0, n, 1);
+	recurPrint(list,firstPrime, n);
+
 	return 0;
 }
 
