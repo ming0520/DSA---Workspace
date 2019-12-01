c6a8cb428f080849e25f831ecdac57f219954384
ming0520 <mingblog0520@gmail.com>
2019-12-01 21:06:13 +0800

    Initialize

---
+++ LabWork1_Q4_CalculatePopulation/src/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : LabWork1_Q4_CalculatePopulation.cpp
+// Author      : Zhong Ming Tan
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
